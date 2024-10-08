3eb4b1c847b3f2fabcfcb86bd5b10397a90d7f5c
Steve Whiteley <steve@memiah.co.uk>
2024-10-08 12:57:01 +0100

    Make sure Utils is referenced with full namespace.

--- lib/vagrant-box-s3/downloader.rb
+++ lib/vagrant-box-s3/downloader.rb
@@ -7,7 +7,7 @@ module Vagrant
       alias_method :original_head, :head
 
       def head
-        if VagrantPlugins::BoxS3::Utils.is_s3_manifest(@source)
+        if ::VagrantPlugins::BoxS3::Utils.is_s3_manifest(@source)
           options, subprocess_options = self.options
           options.unshift("-i")
           options << @source
